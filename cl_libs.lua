470e4828f3b4fd52bed47a8fc6b49ef199cefea7
MBL <unutulmayans_8466@hotmail.com>
2024-11-06 04:38:28 +0300

    Update cl_libs.lua

--- cl_libs.lua
+++ cl_libs.lua
@@ -1,25 +1,23 @@
 MBLFunctions = {}
 -- things that are not in the default lua libraries
-CreateThread(function()
-    local vorpDedected = GetResourceState('vorp_core') == 'started'
-    print("Vorp: " .. tostring(vorpDedected))
-    if vorpDedected then
-        VorpCore = exports.vorp_core:GetCore()
-        MBLFunctions["notify"] = function(text, duration)
-            return VorpCore.NotifyTip(text, duration)
-        end
-        return
+local vorpDedected = GetResourceState('vorp_core') == 'started'
+print("Vorp: " .. tostring(vorpDedected))
+if vorpDedected then
+    VorpCore = exports.vorp_core:GetCore()
+    MBLFunctions["notify"] = function(text, duration)
+        return VorpCore.NotifyTip(text, duration)
     end
-    local rsgDedected = GetResourceState('rsg-core') == 'started'
-    if rsgDedected then
-        print("RSG: " .. tostring(rsgDedected))
-        RSGCore = exports['rsg-core']:GetCoreObject()
-        MBLFunctions["notify"] = function(text,duration)
-            return lib.notify({ title = "Notification", description = text, type = 'inform' })
-        end
-        return
+    return
+end
+local rsgDedected = GetResourceState('rsg-core') == 'started'
+if rsgDedected then
+    print("RSG: " .. tostring(rsgDedected))
+    RSGCore = exports['rsg-core']:GetCoreObject()
+    MBLFunctions["notify"] = function(text,duration)
+        return lib.notify({ title = "Notification", description = text, type = 'inform' })
     end
-end)
+    return
+end
 
 exports("MBLFunctions", function()
     return MBLFunctions
